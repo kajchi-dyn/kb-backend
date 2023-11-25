5dda0c20caa210a0d8f050fcaf681ce184bb33da
Kajchi B <harnest64@gmail.com>
2023-11-25 07:34:10 +0100

    Real real real real real :) for real xd secrets lol

--- secrets.py
+++ secrets.py
@@ -1,8 +1,8 @@
 dyn="media" # but you already know that 
-code="34q06957823dgfasgja234" # but this is probably false
-real_code="love"
-real_real_code="compasion"
-real_x3_code ="community"
-real_x4_code ="dyn"
-real_x5_code ="media"
-real_x6_code ="but you already know that :)"
+code="a4hOKFhwxRuxoQIu1srUKbUt8aNSCl6a" # but this is probably false
+real_code="lE17NYOmSdBE9BKTtLNPZHK0yVatIQ66"
+real_real_code="cbphddP9t3p86OSG8suajdi5hPSsNhDK"
+real_x3_code ="fNKce0knpf0tG8fRgvtApx8yJ3BNAxal"
+real_x4_code ="TU7TWWHYAREYOUREADINGTHIS!!!6ZaAcHdjV" # I already showed you my secrets!!! :sob:
+real_x5_code ="a4hOKFhwxRuxoQIu1srUKbUt8aNSCl6a"
+real_x6_code ="fNKce0knpf0tG8fRgvtApx8yJ3BNAxal"
