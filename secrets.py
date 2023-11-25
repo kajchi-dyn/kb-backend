2dbb705ef0b250b4743aa233113198244a836014
Kajchi B <harnest64@gmail.com>
2023-11-25 07:35:24 +0100

    I dont know what I am doing

--- secrets.py
+++ secrets.py
@@ -1,8 +1,8 @@
-dyn="media" # but you already know that 
-code="a4hOKFhwxRuxoQIu1srUKbUt8aNSCl6a" # but this is probably false
-real_code="lE17NYOmSdBE9BKTtLNPZHK0yVatIQ66"
-real_real_code="cbphddP9t3p86OSG8suajdi5hPSsNhDK"
-real_x3_code ="fNKce0knpf0tG8fRgvtApx8yJ3BNAxal"
-real_x4_code ="TU7TWWHYAREYOUREADINGTHIS!!!6ZaAcHdjV" # I already showed you my secrets!!! :sob:
-real_x5_code ="a4hOKFhwxRuxoQIu1srUKbUt8aNSCl6a"
-real_x6_code ="fNKce0knpf0tG8fRgvtApx8yJ3BNAxal"
+dyn="media123" # but you already know that 
+code="a4hOKFhwxRuxoQIu1srUKbUt8aNSCl6a123" # but this is probably false
+real_code="lE17NYOmSdBE9BKTtLNPZHK0yVatIQ66123"
+real_real_code="cbphddP9t3p86OSG8suajdi5hPSsNhDK123"
+real_x3_code ="fNKce0knpf0tG8fRgvtApx8yJ3BNAxal123"
+real_x4_code ="TU7TWWHYAREYOUREADINGTHIS!!!6ZaAcHdjV123" # I already showed you my secrets!!! :sob:
+real_x5_code ="a4hOKFhwxRuxoQIu1srUKbUt8aNSCl6a123"
+real_x6_code ="fNKce0knpf0tG8fRgvtApx8yJ3BNAxal123"
