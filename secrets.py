349145707501d4ab4d230345061575e8d51ccee9
Kajchi B <harnest64@gmail.com>
2023-11-25 07:18:26 +0100

    Really really added the secrets xd

--- secrets.py
+++ secrets.py
@@ -1,2 +1,8 @@
-no secrets here :)
-All the secrets are in the code :)
+dyn="media" # but you already know that 
+code="34q06957823dgfasgja234" # but this is probably false
+real_code="love"
+real_real_code="compasion"
+real_x3_code ="community"
+real_x4_code ="dyn"
+real_x5_code ="media"
+real_x6_code ="but you already know that :)"
